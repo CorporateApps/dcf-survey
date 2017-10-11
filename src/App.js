05fe432863b6565209019fd5bb26e756b4e58fca
Pieter Jan Geutjens <pieter.geutjens@corporateapps.eu>
2017-10-11 06:45:09 +0200

    initial layout and questions

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import * as Survey from 'survey-react';
 import 'survey-react/survey.css';
 import SurveyEditor from './SurveyEditor';
-import logo from './logo.svg';
+import logo from './proximus.png';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 class App extends Component {
@@ -14,15 +14,21 @@ class App extends Component {
     var model = new Survey.Model(this.json);
     return (
       <div className="App">
+          <div className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <h2>Enterprise Mobility + Security DCF Survey</h2>
+          </div>
         <div className="surveyjs">
           {/*If you want to show survey, uncomment the line below*/}
           <Survey.Survey model={model}/>
           {/*If you want to show survey editor, uncomment the line below*/}
           {/*<SurveyEditor />*/}
         </div>
+          {/*
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+           */}
       </div>
     );
   }
